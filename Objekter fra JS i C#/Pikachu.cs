c0b11bb33b22b68ed38867deb6465c9af40b561d
Lonatri <gyda-94@hotmail.com>
2025-12-01 10:16:51 +0100

    Add project files.

---
+++ Objekter fra JS i C#/Pikachu.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ObjekterFraJSiCSharp
+{
+    internal class PikachuStats
+    {
+        public int Health = 50;
+        public int Level = 21;
+    }
+}
---
+++ Objekter fra JS i C#/Program.cs
@@ -0,0 +1,22 @@
+﻿//using ObjekterFraJSiCSharp;
+//Main();
+//static void Main()
+//{
+//    var Pikachu = new PikachuStats { };
+//    Console.WriteLine($"Pikachu's Health: {Pikachu.Health} and Pikachu's Level: {Pikachu.Level}");
+//}
+//Either way works, as long as the two files are connected.
+
+namespace ObjekterFraJSiCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var Pikachu = new PikachuStats {};
+            Console.WriteLine($"Pikachu's Health: {Pikachu.Health} and Pikachu's Level: {Pikachu.Level}");
+        }
+    }
+}
+
+

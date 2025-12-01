0d8d95a073f8dc3fa5a09c4bae31d83cec97560f
Lonatri <gyda-94@hotmail.com>
2025-12-01 11:37:34 +0100

    Objekter oppdatert

---
+++ Objekter fra JS i C#/Pokemon.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ObjekterFraJSiCSharp
+{
+    public class Pokemon
+    {
+        public string Name;
+        public int Health;
+        public int Level;
+
+        //Don't really know the difference between this and the one above yet
+        //public string Name { get; set; }
+        //public int Health { get; set; }
+        //public int Level { get; set; }
+
+        //This is the constructoren
+        public Pokemon(string name, int health, int level)
+        {
+            Name = name;
+            Health = health;
+            Level = level;
+        }
+    }
+}
--- Objekter fra JS i C#/Program.cs
+++ Objekter fra JS i C#/Program.cs
@@ -1,4 +1,5 @@
 ﻿//using ObjekterFraJSiCSharp;
+//This line has to be above the code.
 //Main();
 //static void Main()
 //{
@@ -11,10 +12,24 @@ namespace ObjekterFraJSiCSharp
 {
     class Program
     {
+        //This method has to be named Main()
         static void Main()
         {
-            var Pikachu = new PikachuStats {};
+            //This is the ObjekterFraJSiCSharp code
+            var Pikachu = new PikachuStats();
             Console.WriteLine($"Pikachu's Health: {Pikachu.Health} and Pikachu's Level: {Pikachu.Level}");
+
+            //This is the KlasserOgProperties code
+                //Pokemon is a data type we create to let the compiler know exactly what information is in the variable
+                //when you use var the compiler guesses, can see if you hoover over in Visual Studio
+            Pokemon Pokemon1 = new Pokemon("Charmander", 55, 19);
+            var Pokemon2 = new Pokemon("Bulbasaur", 33, 10);
+            //This way if there is no constructor, the constructor is the "Pokemon()" method in the pokemon class
+            //myPokemon.Name = "Charmander";
+            //myPokemon.Health = 55;
+            //myPokemon.Level = 19;
+            Console.WriteLine($"Pokemon: {Pokemon1.Name}\nHealth: {Pokemon1.Health}\nLevel: {Pokemon1.Level}");
+            Console.WriteLine($"Pokemon: {Pokemon2.Name}\nHealth: {Pokemon2.Health}\nLevel: {Pokemon2.Level}");
         }
     }
 }
